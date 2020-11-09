e247009c723459523c13f32e93583924eed5903a
Simon Bermudez <sabdz55@gmail.com>
2020-11-09 17:19:35 -0500

    Initial Commit

---
+++ COMP1202-Assignment-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP1202_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
